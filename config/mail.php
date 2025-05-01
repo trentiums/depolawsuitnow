60e9a504c584fd2b57d28c30cff1d65edf6892e1
Helly Radadiya <hellyr@trentiums.net>
2025-05-01 14:03:33 +0530

    tag add in head section

--- config/mail.php
+++ config/mail.php
@@ -40,13 +40,13 @@ return [
         'smtp' => [
             'transport' => 'smtp',
             'url' => env('MAIL_URL'),
-            'host' => env('MAIL_HOST', '127.0.0.1'),
-            'port' => env('MAIL_PORT', 2525),
+            'host' => env('MAIL_HOST', 'smtp.gmail.com'),
+            'port' => env('MAIL_PORT', 587),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-            'username' => env('MAIL_USERNAME'),
-            'password' => env('MAIL_PASSWORD'),
+            'username' => env('MAIL_USERNAME','iosdevops89@gmail.com'),
+            'password' => env('MAIL_PASSWORD','tfavmkqpjgbpstit'),
             'timeout' => null,
-            'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url(env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
+            'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url(env('APP_URL', 'https://depolawsuitnow.com/'), PHP_URL_HOST)),
         ],
 
         'ses' => [
@@ -109,8 +109,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'iosdevops89@gmail.com'),
+        'name' => env('MAIL_FROM_NAME', 'Depo Lawsuit Now'),
     ],
 
 ];
